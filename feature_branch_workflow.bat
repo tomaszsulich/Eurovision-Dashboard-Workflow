3203387aaf6310f19f821c2da14de8c656fd3853
tomek <tomeksulich5@gmail.com>
2026-05-19 22:46:24 +0200

    docs: update workflow script

--- feature_branch_workflow.bat
+++ feature_branch_workflow.bat
@@ -1,5 +1,17 @@
-git clone git@github.com:tomaszsulich/Eurovision-Dashboard-Workflow.git
-git checkout -b feature-readme
+cd C:\Users\tomek
+
+:: Clone repository (run only once)
+IF NOT EXIST Eurovision-Dashboard-Workflow (
+    git clone git@github.com:tomaszsulich/Eurovision-Dashboard-Workflow.git
+)
+
+cd Eurovision-Dashboard-Workflow
+
+git add .
+git commit -m "docs: update workflow script"
+
+:: Create feature branch (skip if branch already exists)
+git checkout feature-readme
 
 echo ## Planned features > projekt.md
 echo - Interactive filters by year and decade >> projekt.md
